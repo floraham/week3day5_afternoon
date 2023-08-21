8e6c189c136a8b67bd38a57c80236fdce32dbda6
floraham <floraham@outlook.com>
2023-08-21 15:02:19 -0700

    ;o

--- dplyr_wrangling.R
+++ dplyr_wrangling.R
@@ -54,11 +54,49 @@ pie_crab3 <- pie_crab %>% filter(site %in% c("NIC", "CC", "PI") & size > 13)
 
 # -------- selecting columns -------- #
 
+# Select individual columns by name, separate them by a comma
+crabs_subset <- pie_crab %>% select(latitude, size, water_temp)
+
+# select range of columns
+crabs_subset2 <- pie_crab %>% select(site:air_temp)
+
+#select a range and an individual column
+crabs_subset <- pie_crab %>% select(date:water_temp, name)
+
+pie_crab %>% select(name, water_temp, size)
+
+#-----mutate!----#
+
+crabs_cm <- pie_crab %>% mutate(size_cm = size / 10)
+
+# what happens if I use mutate to add a new column containing the mean of the size column?
+
+crabs_new_col <- pie_crab %>% mutate(mean_value = mean(pie_crab$size, na.rm= TRUE))
+
+crabs_awesome <- pie_crab %>% mutate(name = "Teddy is awesome")
+
+mean_size_by_site <- pie_crab %>%  group_by(site) %>% summarize(mean_size = mean(size, na.rm = TRUE))
+
+group_mutate <- pie_crab %>%
+  group_by(site) %>%  #recognize that there are discrete levels within each column#
+  mutate(mean_size = mean(size, na.rm = TRUE))
 
 
+#what if i want to create a new column in pie crab that contains giant if the size is greater than 35, or "not giant" if the size is less than or equal to 35?
+
+# use dplyr::case_when() to write if-else statements more easily
+crabs_bin <- pie_crab %>% mutate(size_binned = case_when(
+  size > 35 ~ "giant", #this is my condition and ~ which is what you want to say when it is true
+  size <= 35 ~ "not giant"
+))
 
 
-
+sites_binned <- pie_crab %>%
+  mutate(region = case_when(
+    site %in% c("ZI", "CC", "PIE") ~ "Low",
+    site %in% c("BB", "NIB") ~ "Middle",
+    TRUE ~ "HIGH" #in all other situations, all columns should contain this#
+  ))
 
 
 
